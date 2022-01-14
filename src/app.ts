90c4e43464dd944f07dc3e46db56b5c093d611d7
elsatmr <elsaatamara@gmail.com>
2022-01-13 22:58:56 -0800

    Working server, prior to development

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import SearchController from './controllers/Search.controller';
 
 const app = express();
 const searchRouter = express.Router();
-const testRouter = express.Router()
+const testRouter = express.Router();
 
 searchRouter.get(
   '/api/v1/auth/search/:searchTerm',
@@ -15,5 +15,17 @@ app.get('/', (req: Request, res: Response) => {
   res.status(200).send('Hello World!');
 });
 app.use(searchRouter);
+
+app.get('/songs/Phoenix-TTBC.mp3', (req: Request, res: Response) => {
+  res.status(200).download('./songs/Phoenix-TTBC.mp3');
+});
+
+app.get('/songs/Phoenix-Chloroform.mp3', (req: Request, res: Response) => {
+  res.status(200).download('./songs/Phoenix-Chloroform.mp3');
+});
+
+app.get('/songs/Monolog-Restaurant.mp3', (req: Request, res: Response) => {
+  res.status(200).download('./songs/Monolog-Restaurant.mp3');
+});
 
 export default app;
